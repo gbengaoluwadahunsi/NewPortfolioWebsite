7c4164922c7832997a2b7a99c1cfb9a8ee421ca3
Gbenga Oluwadahunsi <gbengaoluwadahunsi01@gmail.com>
2024-05-16 05:16:45 +0800

    Update: screen.width 1024  responsiveness fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
             Gbénga
           </div>
           {/* primary */}
-          <div className=" hidden  lg:flex md:text-xl lg:text-2xl  justify-center  basis-[86%] gap-12 items-center">
+          <div className=" hidden  xl:flex md:text-xl lg:text-2xl flex-wrap justify-center  basis-[86%] gap-12 items-center">
             <ScrollLink
               className="underline-offset-4 cursor-pointer  hover:underline "
               to="aboutMe"
@@ -70,7 +70,7 @@ const Header = () => {
               Open Source
             </ScrollLink>
             <ScrollLink
-              className="underline-offset-4 cursor-pointer  hover:underline"
+              className="underline-offset-4 cursor-pointer  hover:underline  md:hidden"
               to="contactMe"
               smooth={true}
               duration={2000}
@@ -79,7 +79,7 @@ const Header = () => {
             </ScrollLink>
             
           </div>
-          <button className=" hidden lg:block cursor-pointer w-fit  self-end items-start px-4 text-white py-2 rounded-full shadow-md hover:bg-indigo-700  ">
+          <button className=" hidden xl:block cursor-pointer w-fit  self-end items-start px-4 text-white py-2 rounded-full shadow-md hover:bg-indigo-700  ">
               <a href="src/assets/Images/Gbenga_Oluwadahunsi_CV.pdf" download="">
                 Résumé
               </a>
@@ -87,7 +87,7 @@ const Header = () => {
 
           <div className="w-10">
             <div
-              className=" text-xs lg:hidden cursor-pointer"
+              className=" text-xs xl:hidden cursor-pointer"
               onClick={() => setToggleMenu(!toggleMenu)}
             >
               {toggleMenu ? <XMarkIcon /> : <Bars3Icon />}
@@ -97,14 +97,14 @@ const Header = () => {
         {/* mobile navigation */}
 
         <div
-          className={` fixed z-40 w-[70%] ${"bg-gray-100  text-gray-700"}  overflow-hidden flex flex-col gap-12  origin-top duration-700 ${
-            !toggleMenu ? "h-0" : "h-full w-[64%]  lg:hidden"
+          className={` fixed z-40 w-[70%] md:w-[40%] ${"bg-gray-100  text-gray-700"}  overflow-hidden flex flex-col gap-12  origin-top duration-700 ${
+            !toggleMenu ? "h-0" : "h-full w-[64%]  xl:hidden"
           }`}
         >
           <div className="px-8   h-[30rem] py-16 ">
             <div className="flex  text-[0.8em] flex-col gap-8 font-bold  ">
               <ScrollLink
-                className="  cursor-pointer   "
+                className="cursor-pointer"
                 to="aboutMe"
                 smooth={true}
                 duration={2000}
@@ -157,7 +157,7 @@ const Header = () => {
               >
                 Contact Me
               </ScrollLink>
-              <button className="  md:hidden cursor-pointer w-fit  px-4 text-white py-2 rounded-full shadow-md hover:bg-indigo-700 hover:transition-all duration-300 ">
+              <button className="  xl:hidden cursor-pointer w-fit  px-4 text-white py-2 rounded-full shadow-md hover:bg-indigo-700 hover:transition-all duration-300 ">
                 <a href="src/assets/Images/Gbenga_Oluwadahunsi_CV.pdf" download="">
                   Resume
                 </a>
